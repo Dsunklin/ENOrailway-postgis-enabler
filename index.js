9b6ccc4c310e6ace2e956fc2e86f7034e4e0ee7c
Davily Sunklin (Lil_kelly.) <dsunklin@gmail.com>
2025-12-17 16:10:11 -0800

    Add function to enable PostGIS extension in PostgreSQL

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client } = require('pg');
+
+async function enablePostGIS() {
+  const client = new Client({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to Railway PostgreSQL");
+
+    await client.query("CREATE EXTENSION IF NOT EXISTS postgis;");
+    console.log("PostGIS extension enabled!");
+
+    const res = await client.query("SELECT PostGIS_Version();");
+    console.log("PostGIS version:", res.rows[0].postgis_version);
+
+  } catch (err) {
+    console.error("Error enabling PostGIS:", err);
+  } finally {
+    await client.end();
+  }
+}
+
+enablePostGIS();
